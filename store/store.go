fd4525e7323d350e9d4429c220ce813d1894cab0
dattasaitejatadi <dst8074@gmail.com>
2025-04-01 11:40:45 +0530

    updated pool config

--- store/store.go
+++ store/store.go
@@ -16,9 +16,10 @@ type store struct {
 func New(password string) *store {
 	dsn := fmt.Sprintf("postgresql://postgres.qehkntqehiukjqlbmygr:%s@aws-0-us-west-1.pooler.supabase.com:5432/postgres", password)
 	db, err := sql.Open("postgres", dsn)
-	db.SetMaxOpenConns(15)
-	db.SetMaxIdleConns(15)
-	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetMaxOpenConns(45)                  // Matches Supabase pool size
+	db.SetMaxIdleConns(20)                  // Keep some idle connections to avoid frequent creation
+	db.SetConnMaxLifetime(20 * time.Minute) // Refresh active connections every 20 mins
+	db.SetConnMaxIdleTime(5 * time.Minute)  // Close idle connections after 5 mins
 	if err != nil {
 		log.Fatalf("Could not connect to the database: %v \n issue with %v", err, dsn)
 	}
